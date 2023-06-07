1933379342592006dad5d21372b30016c8826bb7
malik <bittiev.malik@mail.ru>
2023-06-07 16:26:56 +0300

    module5-task1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import FilterView from './view/filter-view.js';
 import {render} from './framework/render';
 import {generateFilter} from './mock/filter';
-import {generateSorter} from './mock/sort';
 import BoardPresenter from './presenter/board-presenter.js';
 import TripPointModel from './model/trip-model.js';
 import { mockInit, tripPoints } from './mock/point.js';
@@ -9,10 +8,10 @@ const pageContainer = document.querySelector('.trip-events');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 
 const filters = generateFilter();
-const sorters = generateSorter();
+
 mockInit(5, 10);
 const tripPointsModel = new TripPointModel(tripPoints);
-const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel, sorters});
+const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel: tripPointsModel});
 render(new FilterView(filters), siteFilterElement);
 
 boardPresenter.init();
