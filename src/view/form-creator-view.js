5db8c30f092ac117c59fe6fcb6ab4f46590e0130
malik <bittiev.malik@mail.ru>
2023-06-08 22:20:07 +0300

    module6-task2

--- src/view/form-creator-view.js
+++ src/view/form-creator-view.js
@@ -1,22 +1,6 @@
 import { destinations } from '../mock/destin.js';
 import AbstractView from '../framework/view/abstract-view.js';
 import { convertToFormDate, capitalizeType, getItemFromItemsById } from '../util.js';
-import { getOffersByType } from '../mock/const.js';
-
-function createOffersTemplate(offers, type) {
-  return getOffersByType(type).map((offer) => {
-    const isOfferChecked = offers.includes(offer.id) ? 'checked' : '';
-    return `
-    <div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${isOfferChecked}>
-      <label class="event__offer-label" for="event-offer-luggage-1">
-        <span class="event__offer-title">${offer.title}</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">${offer.price}</span>
-      </label>
-    </div>`;
-  }).join('');
-}
 
 function createDestinationPicsTemplate(destination) {
   return destination.pictures.map((pic) => `
@@ -111,7 +95,7 @@ function createFormTemplate(tripPoint) {
       <section class="event__section  event__section--offers">
         <h3 class="event__section-title  event__section-title--offers ${visibility}">Offers</h3>
         <div class="event__available-offers">
-          ${createOffersTemplate(tripPoint.offersIDs, tripPoint.type)}
+
         </div>
       </section>
       <section class="event__section  event__section--destination">
